8b50bbe134496467128bf71443b74bed93f5cf48
nainshi-95 <nainshi95@gmail.com>
2026-04-27 11:06:49 +0900

    Update mismatchDebug.cpp

--- mismatchDebug.cpp
+++ mismatchDebug.cpp
@@ -23,3 +23,95 @@ if (isEncoding()
          (int)cu.oppositeLicFlag);
 }
 // --- DEBUG END ---
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if BOUNDARY_COMPENSATE
+  const int orgNumMergeCand = (int)cu->cs->sps->m_maxNumMergeCand;
+  const int loopNumMergeCand = bcMergeCtx ? bcMergeCtx->numCandToTestEnc : mergeCtx.numCandToTestEnc;
+#else
+  const int loopNumMergeCand = mergeCtx.numCandToTestEnc;
+#endif
+
+  for (uint32_t uiMergeCand = 0; uiMergeCand < (uint32_t)loopNumMergeCand; uiMergeCand++)
+  {
+#if BOUNDARY_COMPENSATE
+    const bool useBcCtx = bcMergeCtx && ((int)uiMergeCand >= orgNumMergeCand);
+    const MergeCtx &curMergeCtx = useBcCtx ? *bcMergeCtx : mergeCtx;
+
+    // Extra 영역은 반드시 BC candidate만 허용
+    if (useBcCtx && !curMergeCtx.BCCandFlags[uiMergeCand])
+    {
+      continue;
+    }
+
+    // 원본 영역에서는 BC candidate가 들어오면 안 됨
+    if (!useBcCtx && curMergeCtx.BCCandFlags[uiMergeCand])
+    {
+      continue;
+    }
+#else
+    const MergeCtx &curMergeCtx = mergeCtx;
+#endif
+
+    if (cu->oppositeLicFlag && !curMergeCtx.LICFlags[uiMergeCand] && !cu->cs->sps->m_biLicEnabledFlag &&
+        curMergeCtx.interDirNeighbours[uiMergeCand] == 3)
+    {
+      continue;
+    }
+
+    MergeItem *regularMerge = mergeItemList.allocateNewMergeItem();
+    regularMerge->importMergeInfo(curMergeCtx, uiMergeCand, MergeItem::MergeItemType::REGULAR, *cu);
+
+#if BOUNDARY_COMPENSATE
+    regularMerge->boundaryCompensation = curMergeCtx.BCCandFlags[uiMergeCand];
+#endif
+
+    auto dstBuf = regularMerge->getPredBuf(localUnitArea);
+
+    PelUnitBuf *noMvRefineBuf = nullptr;
+    PelUnitBuf *noCiipBuf     = nullptr;
+
+#if BOUNDARY_COMPENSATE
+    if (!useBcCtx)
+#endif
+    {
+      noMvRefineBuf = mrgPredBufNoMvRefine ? (*mrgPredBufNoMvRefine)[uiMergeCand] : nullptr;
+      noCiipBuf     = mrgPredBufNoCiip ? (*mrgPredBufNoCiip)[uiMergeCand] : nullptr;
+    }
+
+    generateMergePrediction(localUnitArea, regularMerge, *cu, true, true, dstBuf, false, false,
+                            noMvRefineBuf, noCiipBuf, &curMergeCtx);
+
+#if BOUNDARY_COMPENSATE
+    if (useBcCtx && regularMerge->boundaryCompensationMode == BoundaryCompensateFunctions::BC_WARP_NONE)
+    {
+      mergeItemList.discardMergeItem(regularMerge);
+      continue;
+    }
+#endif
+
+    regularMerge->cost = calcLumaCost4MergePrediction(ctxStart, dstBuf, sqrtLambdaForFirstPassIntra, *cu, distParam);
+    mergeItemList.insertMergeItemToList(regularMerge);
+  }
+
+
+
+
+
+
+
